532f23f1a846a7f535546d052c1cb06605c4be5e
Carlos José Díaz Baso <carloskdb@gmail.com>
2025-05-05 09:32:46 +0200

    Update installation instructions, enhance NMF class, and add download script

--- examples/nmf_basis.py
+++ examples/nmf_basis.py
@@ -1,13 +1,12 @@
 import torchmfbd
 
-tmp = torchmfbd.NMF(n_pixel=128,
+tmp = torchmfbd.Basis(n_pixel=128,
                  wavelength=8542.0,
                  diameter=100.0,
                  pix_size=0.059,
                  central_obs=0.0,
-                 n_modes=25,
-                 r0_min=15.0,
-                 r0_max=50.0)
+                 n_modes=150,
+                 r0_min=5.0,
+                 r0_max=30.0)
     
-# psf, modes, r0 = tmp.compute_psfs(100)
-tmp.compute_nmf(100)
+tmp.compute(type='nmf',n=150)
